f7a108ecef2cb6906cbbe58cfaba2dc2f8b02558
JB-Tellez <jb.tellez@gmail.com>
2020-11-20 11:51:52 -0800

    adds stacks/queues demo and tests

---
+++ class-10/demos/stacks_and_queues/stacks_and_queues.py
@@ -0,0 +1,17 @@
+"""Stack, Queue, and the other classes to support them"""
+
+
+class Node:
+    pass
+
+
+class InvalidOperationError(Exception):
+    pass
+
+
+class Stack:
+    pass
+
+
+class Queue:
+    pass
