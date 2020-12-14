721b092b0b8fe75d198af41d6e48c52bef23d01a
JB-Tellez <jb.tellez@gmail.com>
2020-12-14 15:03:31 -0800

    adds tests and navigation

--- class-26/demos/django-snacks/snacks/tests.py
+++ class-26/demos/django-snacks/snacks/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+class SnacksTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_templete(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+    def test_about_page_status_code(self):
+        url = reverse('about')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_about_templete(self):
+        url = reverse('about')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'about.html')
+        self.assertTemplateUsed(response, 'base.html')
