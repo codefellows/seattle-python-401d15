0624cbbe2d2a7f38fd393376a4c081fa94a295b2
JB-Tellez <jb.tellez@gmail.com>
2020-12-15 23:48:11 -0800

    adds rank and priority_eater to model

--- class-27/demos/django-snacks/snacks/models.py
+++ class-27/demos/django-snacks/snacks/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Snack(models.Model):
     name = models.CharField(max_length=64)
+    rank = models.IntegerField(default=100)
+    priority_eater = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
